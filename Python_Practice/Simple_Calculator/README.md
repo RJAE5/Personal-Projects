fde17cfce7dc648cbb4665e2c49073cf27e5ab36
rjae5 <rykirjoe@yahoo.com>
2025-01-15 17:20:36 -0600

    Improve documentation using new style

--- Python_Practice/Simple_Calculator/README.md
+++ Python_Practice/Simple_Calculator/README.md
@@ -2,4 +2,8 @@
 ### Rykir Evans
 ### Description:
 
-This portion of my Python learning journey showcases the first simple project that I decided to embark on. It utlizes a basic interaction with the user to obtain two numbers and perform a desired calculation based on the user's request. It has an basic error handling for operations such as dividing by zero.
+This portion of my Python learning journey showcases the first simple project that I decided to embark on. It utlizes a basic interaction with the user to obtain two numbers and perform a desired calculation based on the user's request. It has an basic error handling for operations such as dividing by zero. This program was made to reinforce the usage of conditionals, simple comparisons, error checking, and formatted strings.
+
+|   #   | File                                     | Description                                  |
+| :---: | ---------------------------------------- | -------------------------------------------- |
+|   1   | [calc.py](./calc.py)                     | Sole driving file for the calculator         |
--- Python_Practice/Simple_Calculator/calc.py
+++ Python_Practice/Simple_Calculator/calc.py
@@ -1,3 +1,38 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#   Author:    Rykir Evans
+#   Email:     rykirjoe@yahoo.com
+#   Title:     Simple Calculator
+#   Repo:      Personal_Projects
+#   Date:      1-15-2025 (Initial Creation)
+#
+#   Description:
+#        This program was made to reinforce the usage of conditionals,
+#        simple comparisons, error checking, and formatted strings. The 
+#        program will prompt the user for two numbers and a desired
+#        operation. It will then perform said operation if no error occurs.
+# 
+#   Usage:
+#        Other than simplistic calculations, this program serves as more
+#        of a learning experience than anything.
+#
+#   Files:
+#        calc.py
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
+###########################################################################
+#               ___  ___      _        ______           _                 #
+#               |  \/  |     (_)       | ___ \         | |                #
+#               | .  . | __ _ _ _ __   | |_/ / ___   __| |_   _           #
+#               | |\/| |/ _` | | '_ \  | ___ \/ _ \ / _` | | | |          #
+#               | |  | | (_| | | | | | | |_/ / (_) | (_| | |_| |          #
+#               \_|  |_/\__,_|_|_| |_| \____/ \___/ \__,_|\__, |          #
+#                                                          __/ |          #
+#                                                         |___/           #
+###########################################################################   
+
 # Prompt for username input
 user_name = input("Please enter your name: ")
 
